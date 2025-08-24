dc09412363ce4c4542ef767aa23ac810974d9f48
Travis Rogers <travis@synergylandpartners.com>
2025-08-24 17:10:48 -0400

    Extract ChartTypeToggle component

--- dashboard/pages/index.js
+++ dashboard/pages/index.js
@@ -7,6 +7,7 @@ import { getWeekStart as getWeekStartHelper, getDateRange as getDateRangeHelper,
 import LoadingSpinner from '../components/common/LoadingSpinner';
 import ErrorMessage from '../components/common/ErrorMessage';
 import MetricCard from '../components/Cards/MetricCard';
+import ChartTypeToggle from '../components/Header/ChartTypeToggle';
 
 const Dashboard = () => {
   const [timeRange, setTimeRange] = useState(TIME_RANGES.CURRENT_WEEK);
@@ -670,16 +671,10 @@ const Dashboard = () => {
               )}
 
               {/* Chart Type Toggle */}
-              <div className="flex items-center space-x-2">
-                <select
-                  value={chartType}
-                  onChange={(e) => setChartType(e.target.value)}
-                  className="border border-gray-300 rounded-md px-3 py-2 bg-white focus:outline-none focus:ring-2 focus:ring-blue-500"
-                >
-                  <option value={CHART_TYPES.DAILY}>Daily View</option>
-                  <option value={CHART_TYPES.WEEKLY}>Weekly View</option>
-                </select>
-              </div>
+              <ChartTypeToggle
+                chartType={chartType}
+                onChartTypeChange={setChartType}
+              />
             </div>
           </div>
         </div>
